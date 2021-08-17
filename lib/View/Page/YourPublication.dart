bec685652b54fb9f705a1fab3cab7e885df02027
Dominique Youness <youness.dominique@gmail.com>
2021-08-17 14:41:24 +0200

    Your publication update

--- lib/View/Page/YourPublication.dart
+++ lib/View/Page/YourPublication.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:safetysecurity/globalsVariables.dart';
 
 class YourPublication extends StatefulWidget{
   @override
@@ -9,11 +10,60 @@ class YourPublication extends StatefulWidget{
 }
 
 class _YourPublication extends State<YourPublication>{
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+
+  @override
+  void initState() {
+
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
+      key: _scaffoldKey,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  backButton(context),
 
+                  SizedBox(width: 15.0,),
+
+                  Text(
+                    'SAFETY SECURITY',
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: width(context) / 15,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ],
+              ),
+
+              SizedBox(height: 20.0),
+
+              Text(
+                'VOS PUBLICATIONS',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontSize: width(context) / 15,
+                ),
+                textHeightBehavior: TextHeightBehavior(applyHeightToFirstAscent: false),
+              ),
+
+              SizedBox(height: 10.0),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
