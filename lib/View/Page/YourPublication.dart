706cabff388ce1ae0ea679dfc70a12c9fcd38c35
Dominique Youness <youness.dominique@gmail.com>
2021-08-17 15:00:54 +0200

    Your place update

--- lib/View/Page/YourPublication.dart
+++ lib/View/Page/YourPublication.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:safetysecurity/Model/Articles.dart';
 import 'package:safetysecurity/globalsVariables.dart';
 
 class YourPublication extends StatefulWidget{
@@ -12,11 +13,13 @@ class YourPublication extends StatefulWidget{
 class _YourPublication extends State<YourPublication>{
 
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
-
+  List<Articles> art;
 
   @override
   void initState() {
-
+    art = articles
+        .where((donnees) => donnees.id.contains(currentFirebaseUser.uid))
+    .toList();
   }
 
   @override
@@ -55,11 +58,49 @@ class _YourPublication extends State<YourPublication>{
                 style: TextStyle(
                   fontFamily: 'Roboto',
                   fontSize: width(context) / 15,
+                  letterSpacing: 2,
                 ),
                 textHeightBehavior: TextHeightBehavior(applyHeightToFirstAscent: false),
               ),
 
               SizedBox(height: 10.0),
+
+              Column(
+                  children: art.map((e){
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          itemArticle(context, _scaffoldKey, e),
+
+                          SizedBox(height: 2.0),
+
+                          TextButton(
+                            onPressed: (){
+
+                            },
+                            child: Container(
+                              height: width(context) / 10,
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                color: Colors.red,
+                                borderRadius: BorderRadius.circular(5.0)
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'SUPPRIMER',
+                                  style: TextStyle(
+                                    color: Colors.white
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  }).toList()
+              ),
             ],
           ),
         ),
